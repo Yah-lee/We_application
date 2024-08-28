8ddc39d827a79acd5d34c1c0f3ba60d16378d657
Yah-lee <yahleeperfectlove@gmail.com>
2024-08-29 05:53:39 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,41 @@
 <template>
-  <v-card class="mx-auto my-16 px-6 py-8 " max-width="344">
-    <v-form v-model="form" @submit.prevent="onSubmit">
-      <v-text-field v-model="email" :readonly="loading" :rules="[required]" class="mb-2" label="Email Or Phone Number"
-        clearable></v-text-field>
-
-      <v-text-field v-model="password" :readonly="loading" :rules="[required]" label="Password"
-        placeholder="Enter your password" clearable></v-text-field>
-
-      <br>
-
-      <v-btn :disabled="!form" :loading="loading" color="success" size="large" type="submit" variant="elevated" block>
-        Sign In
-      </v-btn>
-    </v-form>
+  <v-card class="mx-auto my-16 px-6 py-8" max-width="100%">
+    <v-row>
+      <v-col cols="6" :style="{ backgroundColor: '#D3D3D3' }">
+        <h1>Information</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio ratione corporis eveniet doloribus, accusamus
+          est animi, consequuntur in natus quaerat commodi delectus nisi facilis? Error at quas ad cum officia.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolore neque minima quas saepe incidunt impedit
+          cum esse sunt cupiditate, optio nisi! Maxime officia distinctio sequi adipisci suscipit quaerat recusandae!
+        </p>
+        <v-btn to="/login">Have An Account</v-btn>
+      </v-col>
+      <v-col cols="6" :style="{ backgroundColor: '#003366' }">
+        <h1 class="mx-16">REGISTER FORM </h1>
+        <div class="d-flex justify-center ">
+          <v-text-field class="mx-2" label="First Name" variant="outlined"></v-text-field>
+          <v-text-field class="mx-2" label="Last Name"></v-text-field>
+        </div>
+        <div>
+          <v-text-field label="Email"></v-text-field>
+        </div>
+        <div class="d-flex justify-center">
+          <v-text-field class="mx-2" label="Password"></v-text-field>
+          <v-text-field class="mx-2" label="Confirm Password"></v-text-field>
+        </div>
+        <div class="d-flex ">
+          <v-checkbox></v-checkbox>
+          <p class="my-5">I agree to the <a href="#">terms of service</a></p>
+        </div>
+        <div>
+          <v-btn to="/home">Register</v-btn>
+        </div>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
+
 <script>
 export default {
-  data: () => ({
-    form: false,
-    email: null,
-    password: null,
-    loading: false,
-  }),
-
-  methods: {
-    onSubmit() {
-      if (!this.form) return
-
-      this.loading = true
-
-      setTimeout(() => (this.loading = false), 2000)
-    },
-    required(v) {
-      return !!v || 'Field is required'
-    },
-  },
 }
 </script>
---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex justify-center align-center" style="height: 100vh;">
+    <v-card class="mx-auto px-6" style="width: 1000px; height: 500px">
+      <v-row>
+        <v-cols cols="6">
+          <h4 class="text-center">Login to use your account</h4>
+        </v-cols>
+        <v-cols cols="6"></v-cols>
+      </v-row>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
